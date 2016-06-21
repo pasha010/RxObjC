1b6ace6aa3c5af84a96438e17f63cfcb01040521
Pavel Malkov <mpa026@gmail.com>
2016-06-21 12:09:17 +0300

    forgot header for RxSchedulers

---
+++ RxObjC/Schedulers/RxSchedulers.h
@@ -0,0 +1,21 @@
+//
+//  RxSchedulers.h
+//  RxObjC
+//
+//  Created by Pavel Malkov on 20.06.16.
+//  Copyright (c) 2016 Pavel Malkov. All rights reserved.
+//
+
+#ifndef RxSchedulers_h
+#define RxSchedulers_h
+
+#import <Foundation/Foundation.h>
+
+@class RxAnyRecursiveScheduler;
+
+typedef void (^RxRecursiveImmediateAction)(id, void(^)(id));
+
+typedef void (^RxAnyRecursiveSchedulerAction)(id state, RxAnyRecursiveScheduler *scheduler);
+
+
+#endif /* RxSchedulers_h */
