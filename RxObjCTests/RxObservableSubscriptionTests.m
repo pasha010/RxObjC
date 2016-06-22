76ff27adb44c0469c8315601d19eaf6f2151e64f
Pavel Malkov <mpa026@gmail.com>
2016-06-22 22:23:22 +0300

    complete subscription tests for publish subject

--- RxObjCTests/RxObservableSubscriptionTests.m
+++ RxObjCTests/RxObservableSubscriptionTests.m
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "RxTest.h"
 #import "RxPublishSubject.h"
+#import "RxTestError.h"
 
 @interface RxObservableSubscriptionTests : RxTest
 
@@ -58,6 +59,152 @@
 
     [subscription dispose];
     [publishSubject onNext:@2];
+
+    XCTAssertTrue([lastElement isEqualToNumber:@1]);
+    XCTAssertTrue(lastError == nil);
+    XCTAssertTrue(onNextCalled == 1);
+    XCTAssertTrue(onErrorCalled == 0);
+    XCTAssertTrue(onCompletedCalled == 0);
+    XCTAssertTrue(onDisposedCalled == 1);
+}
+
+- (void)testSubscribeOnError {
+    RxPublishSubject<NSNumber *> *publishSubject = [RxPublishSubject create];
+
+    __block int onNextCalled = 0;
+    __block int onErrorCalled = 0;
+    __block int onCompletedCalled = 0;
+    __block int onDisposedCalled = 0;
+
+    __block NSNumber *lastElement = nil;
+    __block NSError *lastError = nil;
+
+    id <RxDisposable> subscription = [publishSubject subscribeOnNext:^(id o) {
+        lastElement = o;
+        onNextCalled++;
+    } onError:^(NSError *error) {
+        lastError = error;
+        onErrorCalled++;
+    } onCompleted:^{
+        onCompletedCalled++;
+    } onDisposed:^{
+        onDisposedCalled++;
+    }];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == nil);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 0);
+    XCTAssertTrue(onCompletedCalled == 0);
+    XCTAssertTrue(onDisposedCalled == 0);
+
+    [publishSubject onError:[RxTestError testError]];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == [RxTestError testError]);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 1);
+    XCTAssertTrue(onCompletedCalled == 0);
+    XCTAssertTrue(onDisposedCalled == 1);
+
+    [subscription dispose];
+    [publishSubject onNext:@2];
+    [publishSubject onCompleted];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == [RxTestError testError]);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 1);
+    XCTAssertTrue(onCompletedCalled == 0);
+    XCTAssertTrue(onDisposedCalled == 1);
+}
+
+- (void)testSubscribeOnCompleted {
+    RxPublishSubject<NSNumber *> *publishSubject = [RxPublishSubject create];
+
+    __block int onNextCalled = 0;
+    __block int onErrorCalled = 0;
+    __block int onCompletedCalled = 0;
+    __block int onDisposedCalled = 0;
+
+    __block NSNumber *lastElement = nil;
+    __block NSError *lastError = nil;
+
+    id <RxDisposable> subscription = [publishSubject subscribeOnNext:^(id o) {
+        lastElement = o;
+        onNextCalled++;
+    } onError:^(NSError *error) {
+        lastError = error;
+        onErrorCalled++;
+    } onCompleted:^{
+        onCompletedCalled++;
+    } onDisposed:^{
+        onDisposedCalled++;
+    }];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == nil);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 0);
+    XCTAssertTrue(onCompletedCalled == 0);
+    XCTAssertTrue(onDisposedCalled == 0);
+
+    [publishSubject onCompleted];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == nil);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 0);
+    XCTAssertTrue(onCompletedCalled == 1);
+    XCTAssertTrue(onDisposedCalled == 1);
+
+    [subscription dispose];
+    [publishSubject onNext:@1];
+    [publishSubject onError:[RxTestError testError]];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == nil);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 0);
+    XCTAssertTrue(onCompletedCalled == 1);
+    XCTAssertTrue(onDisposedCalled == 1);
+}
+
+- (void)testDisposed {
+    RxPublishSubject<NSNumber *> *publishSubject = [RxPublishSubject create];
+
+    __block int onNextCalled = 0;
+    __block int onErrorCalled = 0;
+    __block int onCompletedCalled = 0;
+    __block int onDisposedCalled = 0;
+
+    __block NSNumber *lastElement = nil;
+    __block NSError *lastError = nil;
+
+    id <RxDisposable> subscription = [publishSubject subscribeOnNext:^(id o) {
+        lastElement = o;
+        onNextCalled++;
+    } onError:^(NSError *error) {
+        lastError = error;
+        onErrorCalled++;
+    } onCompleted:^{
+        onCompletedCalled++;
+    } onDisposed:^{
+        onDisposedCalled++;
+    }];
+
+    XCTAssertTrue(lastElement == nil);
+    XCTAssertTrue(lastError == nil);
+    XCTAssertTrue(onNextCalled == 0);
+    XCTAssertTrue(onErrorCalled == 0);
+    XCTAssertTrue(onCompletedCalled == 0);
+    XCTAssertTrue(onDisposedCalled == 0);
+
+    [publishSubject onNext:@1];
+    [subscription dispose];
+    [publishSubject onNext:@2];
+    [publishSubject onError:[RxTestError testError]];
+    [publishSubject onCompleted];
 
     XCTAssertTrue([lastElement isEqualToNumber:@1]);
     XCTAssertTrue(lastError == nil);
