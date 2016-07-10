2b9d88dcc2412e5737e75f1c26cffe3e254f617c
Pavel Malkov <mpa026@gmail.com>
2016-07-10 20:33:50 +0300

    implement main scheduler tests (100% passed)

--- RxObjCTests/RxMainSchedulerTests.m
+++ RxObjCTests/RxMainSchedulerTests.m
@@ -7,33 +7,102 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "RxTest.h"
+#import "RxMainScheduler.h"
+#import "RxNopDisposable.h"
 
-@interface RxMainSchedulerTests : XCTestCase
+@interface RxMainSchedulerTests : RxTest
 
 @end
 
 @implementation RxMainSchedulerTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+void runRunLoop() {
+    for (NSUInteger i = 0; i < 10; i++) {
+        CFRunLoopRef currentRunLoop = CFRunLoopGetCurrent();
+        dispatch_async(dispatch_get_main_queue(), ^{
+            CFRunLoopStop(currentRunLoop);
+        });
+        CFRunLoopWakeUp(currentRunLoop);
+        CFRunLoopRun();
+    }
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+- (void)testMainScheduler_basicScenario {
+    NSMutableArray<NSNumber *> *messages = [NSMutableArray array];
+    __block BOOL executedImmediatelly = NO;
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+    [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType s) {
+        executedImmediatelly = YES;
+        [messages addObject:@1];
+        [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType o) {
+            [messages addObject:@3];
+            [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType _o) {
+                [messages addObject:@5];
+                return [RxNopDisposable sharedInstance];
+            }];
+            [messages addObject:@4];
+            return [RxNopDisposable sharedInstance];
+        }];
+        [messages addObject:@2];
+        return [RxNopDisposable sharedInstance];
     }];
+
+    XCTAssertTrue(executedImmediatelly);
+
+    runRunLoop();
+
+    NSArray *array = @[@1, @2, @3, @4, @5];
+    XCTAssertTrue([messages isEqualToArray:array]);
 }
 
+- (void)testMainScheduler_disposing1 {
+    NSMutableArray<NSNumber *> *messages = [NSMutableArray array];
+
+    [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType s) {
+        [messages addObject:@1];
+        id <RxDisposable> d3 = [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType o) {
+            [messages addObject:@3];
+            id <RxDisposable> d5 = [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType _o) {
+                [messages addObject:@5];
+                return [RxNopDisposable sharedInstance];
+            }];
+            [d5 dispose];
+            [messages addObject:@4];
+            return d5;
+        }];
+        [messages addObject:@2];
+        return d3;
+    }];
+
+    runRunLoop();
+
+    NSArray *array = @[@1, @2, @3, @4];
+    XCTAssertTrue([messages isEqualToArray:array]);
+}
+
+- (void)testMainScheduler_disposing2 {
+    NSMutableArray<NSNumber *> *messages = [NSMutableArray array];
+
+    [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType o) {
+        [messages addObject:@1];
+        id <RxDisposable> d3 = [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType o) {
+            [messages addObject:@3];
+            id <RxDisposable> d5 = [[RxMainScheduler sharedInstance] schedule:nil action:^id <RxDisposable>(RxStateType o) {
+                [messages addObject:@5];
+                return [RxNopDisposable sharedInstance];
+            }];
+            [messages addObject:@4];
+            return d5;
+        }];
+        [d3 dispose];
+        [messages addObject:@2];
+        return d3;
+    }];
+
+    runRunLoop();
+
+    NSArray *array = @[@1, @2];
+    XCTAssertTrue([messages isEqualToArray:array]);
+}
 @end
