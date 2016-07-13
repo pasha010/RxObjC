376f8123e78efb7dac12eb2cbdeb8cced222f0be
Pavel Malkov <mpa026@gmail.com>
2016-07-13 12:41:21 +0300

    add os version

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-if [ "$OS" == "iOS" ]; then
-  xctool -workspace RxObjc.xcworkspace -scheme AllTests-$1 -sdk $2 -destination name=$3 -configuration DEBUG test
+R_OS=$1
+R_SDK=$2
+R_NAME=$3
+R_OS_VER=$4
+
+if [ R_OS == "iOS" ]; then
+  xctool -workspace RxObjc.xcworkspace -scheme AllTests-R_OS -sdk R_SDK -destination platform='iOS Simulator, OS=R_OS_VER,name=R_NAME' -configuration DEBUG test
 else
-  xctool -workspace RxObjc.xcworkspace -scheme AllTests-$1 -sdk $2 -configuration DEBUG test
+  xctool -workspace RxObjc.xcworkspace -scheme AllTests-R_OS -sdk R_SDK -configuration DEBUG test
 fi
